47503182a995f3fac79d69d6c5d75ad2aa6f5af3
Rubén Gómez García <rgomez@pronoide.es>
2019-04-04 15:45:51 +0000

    Ejemplos spring boot/rest/jpa-data

---
+++ ejemplo13springBoot/src/main/java/com/curso/java/spring/boot/Ejemplo13springBootApplication.java
@@ -0,0 +1,13 @@
+package com.curso.java.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo13springBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo13springBootApplication.class, args);
+	}
+
+}
