a8259a5aeebb5c75117381dceb8f6dd5be109a93
Rubén Gómez García <rgomez@pronoide.es>
2019-04-14 17:10:34 +0000

    Agregado ejemplo para frontend chat

---
+++ ejemploSpringBootWebSocketChat/src/main/java/com/curso/spring/boot/ejemplo15springbootrestclient/EjemploSpringBootWebSocketChatApplication.java
@@ -0,0 +1,13 @@
+package com.curso.spring.boot.ejemplo15springbootrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringBootWebSocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpringBootWebSocketChatApplication.class, args);
+	}
+
+}
